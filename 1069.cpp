49c1835258a7e1162bfe12ad4fc57f2c9247f1ca
Agastya Singh <72365639+a-g-a-s-t-y-a@users.noreply.github.com>
2021-03-02 07:39:00 +0530

    Add files via upload

---
+++ 1069.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    string s;
+    cin>>s;
+    int n=s.length();
+    char c;
+    int len,maxLen;
+    maxLen=-1;
+    c=s[0];len=1;
+    for(int i=1;i<n;i++){
+        if(c==s[i])len++;
+        else{
+            c=s[i];
+            if(len>maxLen) maxLen=len;
+            len=1;
+        }
+    }
+    if(len>maxLen) maxLen=len;
+    cout<<maxLen<<endl;
+}
