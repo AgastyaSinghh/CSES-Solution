dac71c0895ef44c647b2bc4ae96f8e7fb103708b
Agastya Singh <72365639+a-g-a-s-t-y-a@users.noreply.github.com>
2021-03-09 21:39:04 +0530

    Create 1083.cpp

---
+++ 1083.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+ 
+int main() {
+	int n;
+	cin>>n;
+	vector<int> v(n+1,0);
+	int i,c;
+	i=0;
+	while(i<n){
+	    i++;
+	    cin>>c;
+	    v[c]=v[c]|1;
+	}
+	n++;
+	for(i=1;i<n;i++){
+	    if(v[i]==0){
+	        cout<<i<<endl;return 0;
+	    }
+	}
+	
